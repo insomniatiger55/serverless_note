8e8c0852c6d4d4bfe80f91afddcbf83ff4a0b17a
insomniatiger55 <insomniatiger@gmail.com>
2021-04-05 16:03:33 -0500

    commit serverless app

---
+++ sls-notes-backend/api/util.js
@@ -0,0 +1,24 @@
+const getUserId = (headers) => {
+    return headers.app_user_id;
+}
+
+const getUserName = (headers) => {
+    return headers.app_user_name;
+}
+
+const getIdToken = (headers) => {
+    return headers.Authorization;
+}
+
+const getResponseHeaders = () => {
+    return {
+        'Access-Control-Allow-Origin': '*'
+    }
+}
+
+module.exports = {
+    getUserId,
+    getUserName,
+    getIdToken,
+    getResponseHeaders
+}
